49b1bd9807a1a01f6d72a01fd02ec411c258d24b
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-17 01:36:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
-  new_array=[]
+word= ""
 array = string.split
-new_array << array.each do |position|
-  "#{position.reverse}"
+word << array.each do |position|
+  puts "#{position.reverse}"
   end
-  new_array.join
+  puts "#{word}"
 end
